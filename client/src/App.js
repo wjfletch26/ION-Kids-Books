04282597218515e4e6fae56022047daad82ae201
Josh Fletcher <wjfletch26@gmail.com>
2024-11-13 16:39:40 -0600

    first commit with react and tailwind

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <h1 className='text-6xl text-center text-blue-500 font-bold'>
+          Let's get coding</h1>
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the paths according to your project structure
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
